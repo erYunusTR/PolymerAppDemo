86a5d437f271902d8f1459603daabf8144a6951f
yer <yer@hotech.systems>
2019-03-06 20:58:35 +0300

    added all elements

---
+++ src/demos/platinum-elements/platinum-sw/platinum-sw-elements.js
@@ -0,0 +1,22 @@
+import './platinum-sw-cache.js';
+import './platinum-sw-fetch.js';
+import './platinum-sw-import-script.js';
+import './platinum-sw-offline-analytics.js';
+import './platinum-sw-register.js';
+
+/**
+@license
+Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+/* Facilitates importing the full collection of platinum-sw elements. */
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+;
